b57f7acfa8604a79c652a32da32a44fe804f1f42
SHIVA PRASAD <shivaprasad@Mac-111.lan>
2025-02-26 17:41:41 -0500

    updated views.py

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+	username=models.CharField(max_length=100)
+	name=models.CharField(max_length=100)
+	contact=models.CharField(max_length=100)
+	email=models.CharField(max_length=100)
+	password=models.CharField(max_length=100)
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,80 @@
 from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib import messages
+from . models import *
+
 from .GetHash import get_hash
 
+
+def home(request):
+    
+    return render(request, "index.html")
+
+def register(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        contact = request.POST["contact"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        hashed_password = get_hash(password)  # Hash the password
+
+        user = Users.objects.create(name=name, contact=contact, username=email, email=email, password=hashed_password)
+        
+        messages.success(request, "Account created successfully!")
+        
+        return redirect("login")
+
+    return render(request, "register.html")
+
+
+
+
+def login_user(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        print(make_password(password))
+
+        user = Users.objects.filter(username=email).filter(password=get_hash(password)).first()  # Get user by email
+        if user:  # Check hashed password
+
+            request.session['email']=email
+            
+            messages.success(request, "Logged in successfully!")
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid email or password!")
+
+    return render(request, "login.html")
+
+
+
+def dashboard(request):
+    return render(request, "user_home.html")
+
+
+
+def userlogoutdef(request):
+    try:
+        del request.session['email']
+    except:
+        pass
+    return render(request, 'login.html')
+
+
+def userhomedef(request):
+    if "email" in request.session:
+        email=request.session["email"]
+        d=Users.objects.filter(username__exact=email)
+
+       
+        return render(request, 'user_home.html',{'data': d[0]})
+
+    else:
+        return redirect('userlogoutdef')
